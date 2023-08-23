a1514738a4d6a500998cdedfe9fb1a505aa77221
Priyanshu Kumar <65441505+priyanshu102002@users.noreply.github.com>
2023-08-23 13:11:02 +0530

    Update color.js

--- color.js
+++ color.js
@@ -4,7 +4,7 @@ btn.addEventListener('click',function(){
     const r = Math.floor(Math.random()*256);
     const g = Math.floor(Math.random()*256);
     const b = Math.floor(Math.random()*256);
-    const newColor = `rgb(${r},${g},${b})`
+    const newColor = `RGB(${r},${g},${b})`
     document.body.style.backgroundColor = newColor;
     h1.innerText=newColor;
 })
